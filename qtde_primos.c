6a8d9d81235d70da87a53d77d1e65d54bf0c6c09
juolipe <164698014+juolipe@users.noreply.github.com>
2026-09-20 00:59:16 +0000

    Busca e Contagem de Números Primos

--- qtde_primos.c
+++ qtde_primos.c
@@ -10,11 +10,12 @@ int thread_count;
 typedef struct {
     int inicio;
     int fim;
+    int qtde_primos;
 } dados_thread;
 
 int verifica_primo (int num){
     if (num == 0){
-        return 1;
+        return 0;
     }
     if(num == 1){
         return 0;
@@ -33,10 +34,26 @@ int verifica_primo (int num){
     return 1;
 }
 
-/*int main(){
-    int num = 0;
-    verifica_primo(num);
-}*/
+void* conta_primos(void* argc) {
+    dados_thread* dados = (dados_thread*) argc;
+
+    printf("Thread iniciou: %d - %d\n",
+       dados->inicio,
+       dados->fim);
+
+    for(int i = dados->inicio; i <= dados->fim; i++){
+        if(verifica_primo(i)){
+            dados->qtde_primos++;
+        }
+    }
+
+    printf("Thread terminou: %d - %d | Primos: %d\n",
+       dados->inicio,
+       dados->fim,
+       dados->qtde_primos);
+    return NULL;
+}
+
 int main(int argc, char* argv[]){
     pthread_t* thread_handles;
     int limite = strtol(argv[1], NULL, 10);
@@ -46,6 +63,7 @@ int main(int argc, char* argv[]){
 
     int intervalo = limite/thread_count;
 
+  
     for(int i=0; i < thread_count; i++) {
         dados[i].inicio = i * intervalo + 1;
         if(i == thread_count - 1){
@@ -53,6 +71,26 @@ int main(int argc, char* argv[]){
         }else {
             dados[i].fim = (i + 1) * intervalo;
         }
-        pthread_create()
+        dados[i].qtde_primos = 0;
+
+        printf("Criando thread: %d: intervalo %d - %d \n",
+            i,
+            dados[i].inicio,
+            dados[i].fim);
+      
+        pthread_create(&thread_handles[i], NULL, conta_primos, &dados[i]);
     }
+
+    for(int i = 0; i < thread_count; i++) {
+        pthread_join(thread_handles[i], NULL);  
+    }      
+
+    int qtde_primos = 0;
+
+    for(int i = 0; i < thread_count; i++) {
+        qtde_primos += dados[i].qtde_primos;
+    }
+
+    printf("Quantidade números primos de 1 a %d: %d \n", limite, qtde_primos);
+    free(thread_handles);
 }
